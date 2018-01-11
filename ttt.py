8a17731163b21bbbff0476b09bec7f73bc3e380a
Yu-An Ko <handcuffanndo@gmail.com>
2018-01-11 13:24:05 -0500

    add a function to check if p1/p2 wins

--- ttt.py
+++ ttt.py
@@ -1,6 +1,7 @@
 # set up the board
 board_lst = list(range(0,9))
 def draw_board():
+    print("CURRENT BOARD:")
     print("----------")
     print(board_lst[0], "|", board_lst[1], "|", board_lst[2])
     print("----------")
@@ -16,9 +17,19 @@ def if_valid_move(p_input):
     else:
         print("That's not a valid move. Please pick a cell again.\n\n")
 
-# win conditions
+# a list of win combinations
 win_combinations = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
 
+#check if the player wins the game
+def if_win():
+    # iterate through the list of win combinations
+    # return "True" if three cells in a row, column, or diagnal are the same
+    for cell_1, cell_2, cell_3 in win_combinations:
+        if board_lst[cell_1] == board_lst[cell_2] and board_lst[cell_2] == board_lst[cell_3]:
+            print("Congradulations. You win!")
+            return True
+
+# players' symbols
 p1_symbol = "O"
 p2_symbol = "X"
 
@@ -29,10 +40,17 @@ for cell in board_lst:
         p1_input = int(input("Player 1, please try again:"))
     board_lst[p1_input] = p1_symbol
     draw_board()
-
+    # after p1 made the move, check if p1 wins the game
+    # if the returned value is "True", break the loop
+    if if_win():
+        break
     # p2 makes a move
     p2_input = int(input("Player 2, please make a move:"))
     while not if_valid_move(p2_input):
         p2_input = int(input("Player 2, please try again:"))
     board_lst[p2_input] = p2_symbol
     draw_board()
+    # after p2 made the move, check if p2 wins the game
+    # if the returned value is "True", break the loop
+    if if_win():
+        break
