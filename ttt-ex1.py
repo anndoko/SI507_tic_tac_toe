4b22ab12521296da00fea1201e278780369ee565
Yu-An Ko <handcuffanndo@gmail.com>
2018-01-11 23:52:30 -0500

    add a function to prompt the player for input & polish the code using try/except

--- ttt-ex1.py
+++ ttt-ex1.py
@@ -1,5 +1,4 @@
-board_lst = list(range(0,9))
-# function 1. print current board
+# function: print current board
 # params: the draw_board function does not take any input as its parameter
 # return: the draw_board function does not return anything; it prints the current board for the players
 def draw_board():
@@ -12,16 +11,28 @@ def draw_board():
     print(board_lst[6], "|", board_lst[7], "|", board_lst[8])
     print("----------")
 
-# function 2. check the board and see if that is a valid if_valid_move
-# params: the if_valid_move function takes an integer
+# function: prompt the player for input
+# params: the player_move function does not take any input as its parameter
+# return: the player_move function returns an integer
+def player_move():
+    while True:
+        try:
+            player_input = int(input("Please make a move:"))
+            return player_input
+            break
+        except ValueError:
+            print("Please enter the cell number.")
+
+# function: check the board and see if that is a valid if_valid_move
+# params: the if_valid_move function takes an integer as its parameter
 # return: the if_valid_move function returns True if the move is valid
 def if_valid_move(p_input):
     if type(board_lst[p_input]) == int:
         return True
     else:
-        print("That's not a valid move. Please pick a cell again.\n\n")
+        print("That's not a valid move. Please pick a cell again:\n")
 
-# function 3. check if the player wins the game
+# function: check if the player wins the game
 # params: the if_win function does not take any input as its parameter
 # return: the if_win function returns True if any of the win combination is found
 def if_win():
@@ -33,7 +44,7 @@ def if_win():
             print("Congradulations. You win!")
             return True
 
-# function 4. check if it is a tie
+# function: check if it is a tie
 # params: the if_draw function does not take any input as its parameter
 # return: the if_draw returns False if it finds an available cell; otherwise, it returns True
 def if_draw():
@@ -43,15 +54,20 @@ def if_draw():
     print("It is a tie.")
     return True
 
+
+# ---------------------------------------
+# create a list from 0 to 8 for the board
+board_lst = list(range(0,9))
+
 # players' symbols
 p1_symbol = "O"
 p2_symbol = "X"
 
 for cell in board_lst:
     # p1 makes a move
-    p1_input = int(input("Player 1, please make a move:"))
+    p1_input = player_move()
     while not if_valid_move(p1_input):
-        p1_input = int(input("Player 1, please try again:"))
+        p1_input = player_move()
     board_lst[p1_input] = p1_symbol
     draw_board()
     # after p1 made the move, check if p1 wins the game
@@ -59,9 +75,9 @@ for cell in board_lst:
     if if_win() or if_draw():
         break
     # p2 makes a move
-    p2_input = int(input("Player 2, please make a move:"))
+    p2_input = player_move()
     while not if_valid_move(p2_input):
-        p2_input = int(input("Player 2, please try again:"))
+        p2_input = player_move()
     board_lst[p2_input] = p2_symbol
     draw_board()
     # after p2 made the move, check if p2 wins the game
